86029aa6ea3a62172fd8576d333a16487431061b
Ian Greenblott <imgreenblott@gmail.com>
2021-12-20 01:48:25 -0500

    Updated Actions folder. Main reason I am attempting to finishi this assignment  is to reasearvch how I sm going to pull project 3 together.

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,4 +1,12 @@
-export const ADD_CAR = 'ADD_CAR';
-export const REMOVE_CAR = 'REMOVE_CAR';
-export const START_CAR = 'START_CAR';
-export const STOP_CAR = 'STOP_CAR';
+// Products
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// Categories
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+// Cart
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
